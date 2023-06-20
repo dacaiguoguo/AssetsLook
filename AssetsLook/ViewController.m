63001c65b52f0b4ebc20c4212df3e3c38cb42124
sunyanguo <dacaiguoguo@163.com>
2023-06-20 10:45:41 +0800

    commit

--- AssetsLook/ViewController.m
+++ AssetsLook/ViewController.m
@@ -6,7 +6,7 @@
 //
 
 #import "ViewController.h"
-
+#import "SDImageCache.h"
 @interface ViewController ()
 
 @end
@@ -15,8 +15,35 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSURL *url = [NSBundle.mainBundle URLForResource:@"Assets.carInfo" withExtension:@"json"];
+    NSData *data = [NSData dataWithContentsOfURL:url];
+    NSArray *assets = [self parseJsonFromData:data];
+
+    NSMutableArray *imageAssets = [NSMutableArray array];
+    for (NSDictionary *item in assets) {
+        if([[item objectForKey:@"AssetType"] isEqualToString:@"Image"]) {
+            [imageAssets addObject:item];
+        }
+    }
+
+    for (NSDictionary *item in imageAssets) {
+        NSString *name = [item objectForKey:@"Name"];
+        NSString *RenditionName = [item objectForKey:@"RenditionName"];
+
+
+        UIImage *image = [UIImage imageNamed:name];
+        [SDImageCache.sharedImageCache storeImage:image forKey:RenditionName toDisk:YES];
+    }
+
     // Do any additional setup after loading the view.
 }
 
-
+- (id)parseJsonFromData:(NSData *)data {
+    NSParameterAssert(data);
+    id object = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments | NSJSONReadingMutableContainers error:NULL];
+    if ([object isKindOfClass:[NSNull class]]) {
+        return nil;
+    }
+    return object;
+}
 @end
