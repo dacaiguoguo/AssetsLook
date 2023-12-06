951610acdfe793874032e5a875bc3279c1436b34
sunyanguo <dacaiguoguo@163.com>
2023-12-06 16:10:14 +0800

    long double_num

--- AssetsLook/main.m
+++ AssetsLook/main.m
@@ -8,7 +8,18 @@
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
+long double_num(long num) {
+    __asm__ __volatile__(
+                         "lsl x0, x0, 1\n"
+                         "str x0, [sp, #8]\n"
+                         );
+    return num;
+}
+
+
 int main(int argc, char * argv[]) {
+    long ret = double_num(0x68449035);
+    printf("%0ld", ret);
     NSString * appDelegateClassName;
     @autoreleasepool {
         // Setup code that might create autoreleased objects goes here.
