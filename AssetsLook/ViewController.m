c60b48b13940f9d1ce0ac5410f18f69c33a704d3
sunyanguo <dacaiguoguo@163.com>
2023-12-06 13:16:13 +0800

    优化内存

--- AssetsLook/ViewController.m
+++ AssetsLook/ViewController.m
@@ -15,27 +15,63 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSURL *url = [NSBundle.mainBundle URLForResource:@"Assets.carInfo" withExtension:@"json"];
-    NSData *data = [NSData dataWithContentsOfURL:url];
-    NSArray *assets = [self parseJsonFromData:data];
+    NSArray *allBundles =  [self allBundle];
 
-    NSMutableArray *imageAssets = [NSMutableArray array];
-    for (NSDictionary *item in assets) {
-        if([[item objectForKey:@"AssetType"] isEqualToString:@"Image"]) {
-            [imageAssets addObject:item];
+    for (NSBundle *aBundle in allBundles) {
+        NSURL *url = [aBundle.bundleURL URLByAppendingPathComponent:@"Assets.carInfo.json"];
+        NSFileManager *fileManager = [NSFileManager defaultManager];
+        if (![fileManager fileExistsAtPath:url.path]) {
+            continue;
+        }
+        NSData *data = [NSData dataWithContentsOfURL:url];
+        NSArray *assets = [self parseJsonFromData:data];
+        NSMutableArray *imageAssets = [NSMutableArray array];
+        for (NSDictionary *item in assets) {
+            if([[item objectForKey:@"AssetType"] isEqualToString:@"Image"]) {
+                [imageAssets addObject:item];
+            }
+            if([[item objectForKey:@"AssetType"] isEqualToString:@"PackedImage"]) {
+                [imageAssets addObject:item];
+            }
+            for (NSDictionary *item in imageAssets) {
+                NSString *name = [item objectForKey:@"Name"];
+                NSString *RenditionName = [item objectForKey:@"RenditionName"];
+                @autoreleasepool {
+                    UIImage *image = [UIImage imageNamed:name inBundle:aBundle compatibleWithTraitCollection:nil];
+                    NSString *kk = [NSString stringWithFormat:@"%@_%@", aBundle.bundleURL.lastPathComponent, RenditionName];
+                    [SDImageCache.sharedImageCache storeImage:image forKey:kk toDisk:YES];
+                }
+            }
+        }
+
+    }
+}
+
+- (NSArray<NSBundle *> *)allBundle {
+    NSMutableArray *ret = [NSMutableArray arrayWithObject:[NSBundle mainBundle]];
+
+    // 获取主 bundle 的路径
+    NSString *mainBundlePath = [[NSBundle mainBundle] bundlePath];
+
+    // 创建 NSFileManager 对象
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+
+    // 获取主 bundle 目录下所有文件和目录的列表
+    NSArray *contents = [fileManager contentsOfDirectoryAtPath:mainBundlePath error:nil];
+
+    // 遍历所有文件和目录
+    for (NSString *item in contents) {
+        // 判断是否为 bundle 目录
+        if ([item.pathExtension isEqualToString:@"bundle"]) {
+            // 构建 bundle 的完整路径
+            NSString *bundlePath = [mainBundlePath stringByAppendingPathComponent:item];
+
+            // 创建 NSBundle 对象
+            NSBundle *subBundle = [NSBundle bundleWithPath:bundlePath];
+            [ret addObject:subBundle];
         }
     }
-
-    for (NSDictionary *item in imageAssets) {
-        NSString *name = [item objectForKey:@"Name"];
-        NSString *RenditionName = [item objectForKey:@"RenditionName"];
-
-
-        UIImage *image = [UIImage imageNamed:name];
-        [SDImageCache.sharedImageCache storeImage:image forKey:RenditionName toDisk:YES];
-    }
-
-    // Do any additional setup after loading the view.
+    return ret;
 }
 
 - (id)parseJsonFromData:(NSData *)data {
